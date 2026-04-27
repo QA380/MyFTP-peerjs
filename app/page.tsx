49d8fd5bbc3f3cfb9641fc32a4f0d4300ba670a4
Vincent <vincentaewennugrahaa@gmail.com>
2026-04-27 14:37:56 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1480,11 +1480,11 @@ export default function Home() {
         return;
       }
 
-      const constraints =
-        kind === "video" ? { audio: true, video: true } : { audio: true, video: false };
+      // const constraints =
+      //   kind === "video" ? { audio: true, video: true } : { audio: true, video: false };
 
       try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const stream = await navigator.mediaDevices.getUserMedia();
         setLocalStream(stream);
 
         if (mediaConnRef.current) {
